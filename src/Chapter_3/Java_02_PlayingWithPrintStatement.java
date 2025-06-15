b6fc5f0d991994c9a7b6b04c1cc0a8d822fee64f
ShivamAttri05 <shivamattri.842@gmail.com>
2025-06-15 13:40:31 +0530

    Chapter - 3 Update

---
+++ src/Chapter_3/Java_02_PlayingWithPrintStatement.java
@@ -0,0 +1,11 @@
+package Chapter_3;
+
+public class Java_02_PlayingWithPrintStatement {
+    public static void main(String[] args) {
+        System.out.println("4 + 4");            // 4 + 4
+        System.out.println(4 + 4);              // 8
+        System.out.println(4 + 4 + "4");        // 84
+        System.out.println(4 + "4" + 4);        // 444
+        System.out.println("4" + 4 + 4);        // 444
+    }
+}
