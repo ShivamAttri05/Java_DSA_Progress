16573e0f2c1059a720f902e45c787055556af11a
ShivamAttri05 <shivamattri.842@gmail.com>
2025-06-15 13:01:53 +0530

    Chapter - 2 Update

---
+++ src/Chapter_2/Java_01_Variables.java
@@ -0,0 +1,13 @@
+package Chapter_2;
+
+public class Java_01_Variables {
+    public static void main(String[] args) {
+        // Declaring Variable
+        int a;
+        // Assigning Value
+        a = 20;
+
+        // Printing it
+        System.out.println("The value of a is " + a);
+    }
+}
