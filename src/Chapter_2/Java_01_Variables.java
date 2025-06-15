8367e4e5b849e708eb5de7e010b97eecccb56624
ShivamAttri05 <shivamattri.842@gmail.com>
2025-06-15 13:20:50 +0530

    Chapter - 2 Update

--- src/Chapter_2/Java_01_Variables.java
+++ src/Chapter_2/Java_01_Variables.java
@@ -9,5 +9,19 @@ public class Java_01_Variables {
 
         // Printing it
         System.out.println("The value of a is " + a);
+
+        // There are a lot of Data Types exists in Java
+        /*
+        Integer (int)
+        LongInteger (long) - Write 'L' in the end of the LongInteger variable
+
+        Boolean (boolean) - True/False
+
+        Floating Point (float) - Write 'f' in the end of the floating point variable
+        Double (double)
+
+        Character (char)
+        String (String)
+        */
     }
 }
