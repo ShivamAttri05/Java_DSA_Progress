cb5f14af22b63c5963af238c14bb2ec3c3950896
ShivamAttri05 <shivamattri.842@gmail.com>
2025-06-15 12:14:16 +0530

    Initial commit for Java DSA Progress

---
+++ src/Chapter_1/Java_01_Boilerplate.java
@@ -0,0 +1,5 @@
+package Chapter_1;
+
+public class Java_01_Boilerplate {
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Shivam Attri");
+        System.out.println("Today, I welcome you all to this amazing series of Java With DSA...");
+    }
+}
