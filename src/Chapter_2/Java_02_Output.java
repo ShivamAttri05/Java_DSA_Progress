5a2693bccaea22241370256d30721df382f05d35
ShivamAttri05 <shivamattri.842@gmail.com>
2025-06-15 13:14:57 +0530

    Chapter - 2 Update

---
+++ src/Chapter_2/Java_02_Output.java
@@ -0,0 +1,10 @@
+package Chapter_2;
+
+public class Java_02_Output {
+    public static void main(String[] args) {
+        System.out.println("Shivam Attri");
+        // This will exact print whatever written in these brackets () and then into enters next line
+        System.out.print("I Wish to became a Software Engineer");
+        // This will exact print whatever written in these brackets () but does not enter into next line
+    }
+}
